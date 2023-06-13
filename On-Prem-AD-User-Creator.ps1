d02eb52f6c038ced2b0be888058402ae98d1b928
Digressive <digressive@outlook.com>
2023-06-13 22:32:43 +0100

    Update On-Prem-AD-User-Creator.ps1

--- On-Prem-AD-User-Creator.ps1
+++ On-Prem-AD-User-Creator.ps1
@@ -4,7 +4,7 @@
 
 .GUID eaaca86c-2a1f-4caf-b2f9-05868186d162
 
-.AUTHOR Mike Galvin Contact: mike@gal.vin / twitter.com/mikegalvin_ / discord.gg/5ZsnJ5k
+.AUTHOR Mike Galvin Contact: mike@gal.vin 
 
 .COMPANYNAME Mike Galvin
 
